0e3282502c31e50bc063ff0615b7a1c249f66244
Dmitry Ponyatov <dponyatov@gmail.com>
2016-07-31 17:49:06 +0400

    + asm com.com: nop int
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -3,10 +3,15 @@
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
 
 FILE *com = fopen("com.com","wb");
-long org=0;
+
+long org=0x100;
+long I=0;
+char M[Msz];
+
 int main() {
 	assert(com!=NULL);
 	yyparse();
+	fwrite(M,1,I,com);
 	fclose(com);
 	return 0;
 }
--- hpp.hpp
+++ hpp.hpp
@@ -13,10 +13,14 @@ extern int yylineno;
 extern char* yytext;
 #define TOS(X) {yylval.s = new string(yytext); return X;}
 #define HEX(X) {yylval.n = strtol(yytext,NULL,0x10); return X;}
+#define DEC(X) {yylval.n = strtol(yytext,NULL,0x0A); return X;}
 extern int yyparse();
 extern void yyerror(string);
 #include "ypp.tab.hpp"
 
 extern long org;
+extern long I;
+#define Msz 0x10000
+extern char M[Msz];
 
 #endif // _H_HPP
